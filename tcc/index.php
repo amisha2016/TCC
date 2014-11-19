d5ff7d5575377063f12455299a2c74566c952830
Kamal Kaur <kamal.kaur188@gmail.com>
2014-11-20 00:49:09 +0530

    Adds contact details in footer

--- tcc/index.php
+++ tcc/index.php
@@ -206,19 +206,22 @@
 			<footer id="footer">
 
 				<!-- Icons -->
-					<ul class="actions">
+					<ul class="actions" style="line-height:3em">
 						<li><a href="https://twitter.com/tccgndec" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="https://www.facebook.com/pages/Testing-and-Consultancy-Cell-GNDEC-Ludhiana/497681330318981" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
 						<li><a href="https://plus.google.com/u/0/117256570032896355648" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
 						<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
 						<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
 						<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-					</ul>
+					</ul> <div class="rights" style="float:left; margin-top:20px;">© 2014 Testing & Consultancy Cell. All rights reserved.</div>
 
 				<!-- Menu -->
-					<ul class="menu">
-						<li>© 2014 Testing & Consultancy Cell. All rights reserved.</li>
-					</ul>
+					<ul class="menu" style="display:inline">
+                                                
+                                                <li>Email: tcc@gndec.ac.in </li>
+                                                <li>Contact: +91 9855225007</li> <br>	
+                                                </ul>
+					
 			
 			</footer>
 
